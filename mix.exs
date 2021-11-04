eee53b68d1de98092360aeffea23f45968d700be
Praveen Kumar Verma <praveenkumarverma121@gmail.com>
2021-11-05 00:22:52 +0530

    update mix package

--- mix.exs
+++ mix.exs
@@ -7,7 +7,25 @@ defmodule Click.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/Sonatrix/Click",
+      description: description(),
+      package: package()
+    ]
+  end
+
+  defp description() do
+    "Process a set of IP clicks based on constraint"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "click",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README* test),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/Sonatrix/Click"}
     ]
   end
 
